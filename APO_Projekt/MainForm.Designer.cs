ec6f7db0e1217cf6a9d3698ab0a27d62566adf00
Daniel Delegacz <ddelegacz@gmail.com>
2021-04-07 13:29:10 +0200

    Add MenuStrip and Create OpenPicture class

--- APO_Projekt/MainForm.Designer.cs
+++ APO_Projekt/MainForm.Designer.cs
@@ -29,47 +29,103 @@ namespace APO_Projekt
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.test1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test13ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test21ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.test22ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // menuStrip1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(360, 312);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.test1ToolStripMenuItem,
+            this.test2ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // pictureBox2
+            // test1ToolStripMenuItem
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(636, 12);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(360, 312);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.test1ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.test11ToolStripMenuItem,
+            this.test12ToolStripMenuItem,
+            this.test13ToolStripMenuItem});
+            this.test1ToolStripMenuItem.Name = "test1ToolStripMenuItem";
+            this.test1ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.test1ToolStripMenuItem.Text = "test1";
+            // 
+            // test11ToolStripMenuItem
+            // 
+            this.test11ToolStripMenuItem.Name = "test11ToolStripMenuItem";
+            this.test11ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.test11ToolStripMenuItem.Text = "test11";
+            // 
+            // test12ToolStripMenuItem
+            // 
+            this.test12ToolStripMenuItem.Name = "test12ToolStripMenuItem";
+            this.test12ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.test12ToolStripMenuItem.Text = "test12";
+            // 
+            // test13ToolStripMenuItem
+            // 
+            this.test13ToolStripMenuItem.Name = "test13ToolStripMenuItem";
+            this.test13ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.test13ToolStripMenuItem.Text = "test13";
+            // 
+            // test2ToolStripMenuItem
+            // 
+            this.test2ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.test21ToolStripMenuItem,
+            this.test22ToolStripMenuItem});
+            this.test2ToolStripMenuItem.Name = "test2ToolStripMenuItem";
+            this.test2ToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.test2ToolStripMenuItem.Text = "test2";
+            // 
+            // test21ToolStripMenuItem
+            // 
+            this.test21ToolStripMenuItem.Name = "test21ToolStripMenuItem";
+            this.test21ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.test21ToolStripMenuItem.Text = "test21";
+            // 
+            // test22ToolStripMenuItem
+            // 
+            this.test22ToolStripMenuItem.Name = "test22ToolStripMenuItem";
+            this.test22ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.test22ToolStripMenuItem.Text = "test22";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 550);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindow";
             this.Text = "APO";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem test1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test11ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test12ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test13ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test21ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem test22ToolStripMenuItem;
     }
 }
 
